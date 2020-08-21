375a0627d2d67fe7e01a37334db1b230a6da6f92
marcentusch <marcentusch@hotmail.com>
2020-08-21 20:53:39 +0200

    added tasks for minifying css, html and images, setup basic eleventy

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+// Transforms
+const htmlMinTransform = require("./src/transforms/html-min-transform.js");
+
+// Create a helpful production flag
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = (config) => {
+  // Only minify HTML if we are in production because it slows builds _right_ down
+  if (isProduction) {
+    config.addTransform("htmlmin", htmlMinTransform);
+  }
+
+  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  config.setUseGitIgnore(false);
+
+  return {
+    // Setting the template engine to nunjucks. Now .html files can use nunjucks
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,13 @@
+---
+title: "spark"
+layout: "layouts/home.html"
+intro:
+  main: "Bread & Butter"
+  summary: "Whether you’re running a vintage, building your perfect café racer or just making your daily motorcycle as reliable as it can be.
+
+We got you covered from €99"
+  buttonText: "preorder"
+  buttonUrl: "/preorder"
+  image: "/images/bg/bike.jpg"
+  imageAlt: "Buttered toasted white bread"
+---
