64783af87ef92a3301e2ac49cccbd88036a0033d
marcentusch <marcentusch@hotmail.com>
2020-08-23 06:46:21 +0200

    fix service worker path

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,3 @@
-// Register service worker
-// Check if service workers is supported in the browser
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/serviceworker.js")
-      // Service Worker registered successfully
-      .then((reg) => {
-        return;
-      })
-      // Service Worker failed
-      .catch((err) => {
-        return;
-      });
-  });
-}
-
 // Timelines
 const mainTl = gsap.timeline();
 const arrowTl = gsap.timeline();
